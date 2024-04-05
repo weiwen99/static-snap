a996af7d4ba7be1f0840f8e075b6666f4af73b9d
Weiwen Zhang <weiwen@weiwen.org>
2024-04-06 02:32:49 +0800

    stage

--- app/src/simple/FileService.scala
+++ app/src/simple/FileService.scala
@@ -14,4 +14,19 @@ class FileService(root: Path) {
       .toList
       .map { d => FileMeta(root = root, path = d) }
   }
+
+  def listDir(path: Path, sortBy: SortBy): List[FileMeta] = {
+    val files = listDir(path)
+    val rs1   = sortBy.column match
+      case SortColumn.Name         => files.sortBy(_.name)
+      case SortColumn.Size         => files.sortBy(_.size)
+      case SortColumn.Type         => files.sortBy(_.`type`)
+      case SortColumn.LastModified => files.sortBy(_.lastModifiedTime)
+      case SortColumn.LastAccess   => files.sortBy(_.lastAccessTime)
+      case SortColumn.Creation     => files.sortBy(_.creationTime)
+    val rs2   = sortBy.order match
+      case SortOrder.Asc  => rs1
+      case SortOrder.Desc => rs1.reverse
+    rs2
+  }
 }
